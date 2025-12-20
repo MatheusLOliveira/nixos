a045bd922603d78d9f89a549f0f06ef4c8d4d8b6
MatheusLOliveira <matheuslimaoliveira@gmail.com>
2025-12-20 12:37:16 -0300

    add waybar config

--- hosts/common/global/display-managers/hyprland/hyprland-home.nix
+++ hosts/common/global/display-managers/hyprland/hyprland-home.nix
@@ -1,33 +1,31 @@
-{ pkgs, ... }: 
+{ pkgs, ... }:
 {
   # Habilita o módulo do Hyprland no Home Manager
   wayland.windowManager.hyprland = {
     enable = true;
-    
+
     # Isso injeta o conteúdo do seu hyprland.conf dentro da config gerada pelo Nix
     extraConfig = builtins.readFile ./hypr/hyprland.conf;
 
     settings = {
-     binde = [
-       ", XF86AudioRaiseVolume, exec, wpctl set-volume -l 1.5 @DEFAULT_AUDIO_SINK@ 5%+"
-       ", XF86AudioLowerVolume, exec, wpctl set-volume @DEFAULT_AUDIO_SINK@ 5%-"
-     ];
+      binde = [
+        ", XF86AudioRaiseVolume, exec, wpctl set-volume -l 1.5 @DEFAULT_AUDIO_SINK@ 5%+"
+        ", XF86AudioLowerVolume, exec, wpctl set-volume @DEFAULT_AUDIO_SINK@ 5%-"
+      ];
 
-     bind = [
-       ", XF86AudioMute, exec, wpctl set-mute @DEFAULT_AUDIO_SINK@ toggle"
-     ]; 
+      bind = [
+        ", XF86AudioMute, exec, wpctl set-mute @DEFAULT_AUDIO_SINK@ toggle"
+      ];
     };
   };
 
-  # Para arquivos adicionais (hyprlock, scripts, cores, etc), 
-  # nós usamos o xdg.configFile para colocá-los no lugar certo (~/.config/hypr/...)
-  
-  # Exemplo para o hyprlock:
+  #configFile"destiny".source = "where_is_the_file"
+  xdg.configFile."waybar".source = ./hypr/waybar;
   xdg.configFile."hypr/hyprlock.conf".source = ./hypr/hyprlock.conf;
-  
+
   # Exemplo se você tiver uma pasta de scripts dentro de hypr:
   # xdg.configFile."hypr/scripts".source = ./hypr/scripts;
-  
+
   # Pacotes extras que o Hyprland pode precisar e você usa na config
   home.packages = with pkgs; [
     hyprlock
@@ -37,22 +35,22 @@
     waybar
     nautilus
     pamixer
-    
-    wl-clipboard              # Essencial para copiar/colar
-    swaynotificationcenter    # Central de notificações
-    hyprshot                  # Para prints
-    nwg-look                  # Para configurar temas GTK visualmente
-    stow                      # Pode manter se ainda usar para outras coisas fora do Nix
-    
+
+    wl-clipboard # Essencial para copiar/colar
+    swaynotificationcenter # Central de notificações
+    hyprshot # Para prints
+    nwg-look # Para configurar temas GTK visualmente
+    stow # Pode manter se ainda usar para outras coisas fora do Nix
+
     # Agentes de autenticação (polkit)
-    hyprpolkitagent           # Ou outro como polkit-gnome / lxsession
-    
+    hyprpolkitagent # Ou outro como polkit-gnome / lxsession
+
     # --- Tema Catppuccin (com a configuração que você tinha) ---
     (catppuccin-gtk.override {
       accents = [ "blue" ];
       variant = "mocha";
     })
-    
+
     # Ícones e Cursores (opcionais, mas recomendados já que você instalou o tema)
     catppuccin-cursors.mochaBlue
     papirus-icon-theme
